def9e25fffd43a93289e5f5e38ffa5921dae18bb
Kit Burton-Senior <kit.senior@gmail.com>
2016-09-07 00:29:32 +0100

    improved validation and error message

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -2,6 +2,8 @@
 
 namespace NotificationChannels\ClockworkSms\Exceptions;
 
+use NotificationChannels\ClockworkSms\ClockworkSmsMessage;
+
 class CouldNotSendNotification extends \Exception
 {
     public static function serviceRespondedWithAnError($response)
@@ -14,12 +16,18 @@ class CouldNotSendNotification extends \Exception
 
     public static function invalidMessageObject($message)
     {
-        if (! $message->getMessage()->getNumber()) {
-            $message = 'No number set in the message.';
-        } elseif (! $message->getMessage()->getContent()) {
-            $message = 'No content set in the message.';
-        } else {
-            $message = 'The message is not valid.';
+        if ($message instanceof ClockworkSmsMessage) {
+            if (! $message->getMessage()->getNumber()) {
+                $message = 'Notification was not sent. No number set in the message.';
+            } elseif (! $message->getMessage()->getContent()) {
+                $message = 'Notification was not sent. No content set in the message.';
+            } else {
+                $message = 'Notification was not sent. Message is invalid.';
+            }
+        }
+        else {
+            $className = get_class($message) ?: 'Unknown';
+            $message = "Notification was not sent. Message object class `{$className}` is invalid. It must be `".ClockworkSmsMessage::class.'`.';
         }
 
         return new static($message);
